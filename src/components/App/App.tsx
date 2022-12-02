e22e2a698dd5a185aeea86aad54e3b697e21e231
Murad Ibragimov <texet0598@gmail.com>
2022-12-02 17:44:14 +0300

    base structure of the app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import styles from './App.module.scss';
+import 'gapi';
+function App({}) {
+
+  
+  return (
+
+    <div className={styles.container}>
+      <button
+      >Login</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.scss';
+
+function Header({}) {
+  return (
+    <header className={styles.header}>
+
+    </header>
+  );
+}
+
+export default Header;
